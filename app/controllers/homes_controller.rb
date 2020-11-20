b41e8f173d221f662fbc7a277e1fc6d92c201db6
EC2 Default User <kapibara.bba0924@gmail.com>
2020-11-20 11:41:22 +0000

    [Add]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,12 @@ class HomesController < ApplicationController
 
   def top
   end
-
+  
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
 end
