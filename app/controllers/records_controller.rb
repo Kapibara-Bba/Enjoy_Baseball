54e987cc9cd4535bdfde497daa1531048c2d945c
EC2 Default User <kapibara.bba0924@gmail.com>
2020-11-23 09:24:47 +0000

    [update]record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,19 +1,22 @@
 class RecordsController < ApplicationController
-  
+
   def batter_index
     @user = User.all
     @records = Record.all
   end
-  
+
   def pitch_index
     @user = User.all
     @records = Record.all
   end
-  
+
   def create
+    @user = User.find(params[:id])
     Record.create(record_params)
+    Record.save
+    redirect_to user_path(@user)
   end
-  
+
   private
   def record_params
     params.require(:record).permit(:bat, :batting, :hit, :two_base_hit, :three_base_hit, :homerun, :strike_out, :ball, :bunt, :dot, :homein,
