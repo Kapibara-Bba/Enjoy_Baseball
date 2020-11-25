d73830395b35980e72cba237c64fa9f39b003126
EC2 Default User <kapibara.bba0924@gmail.com>
2020-11-25 17:08:33 +0000

    [update]user

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   resources :teams
-  resources :users
+  resources :users, only: [:index, :show, :edit, :update]
   get 'records/batter' => 'records#batter_index'
   get 'records/pitch' => 'records#pitch_index'
   resources :records, only: [:new, :create]
