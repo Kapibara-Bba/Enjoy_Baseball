eb3f6603347eb03907d11d0f5361d95b20e3cacf
EC2 Default User <kapibara.bba0924@gmail.com>
2020-12-10 10:58:00 +0000

    [Add]layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
 
    def after_sign_in_path_for(resource)
-      flash[:notice] = "ログインしました"
-      team_path(current_user.team_id)
+      if current_user.team_id  == nil
+        new_team_path
+      else
+        flash[:notice] = "ログインしました"
+        team_path(current_user.team_id)
+      end
    end
 
    def after_sign_out_path_for(resource)
