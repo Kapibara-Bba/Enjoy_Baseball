cb88e7f440c313c677dfbc224ccf336340f16a7d
EC2 Default User <kapibara.bba0924@gmail.com>
2020-12-21 07:45:22 +0000

    [Add]イベントの追加画面のレスポンシブ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:comment, :image, :video)
+    params.require(:post).permit(:comment, :image)
   end
 
 end
