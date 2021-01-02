69745c7ada7a4882ee9b1f7d1431c47d44f7ea58
EC2 Default User <kapibara.bba0924@gmail.com>
2021-01-02 06:27:09 +0000

    [Add]通知モデル追加

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,6 +2,7 @@ class PostComment < ApplicationRecord
 
   belongs_to :user, optional: true
   belongs_to :team, optional: true
+  has_many :notifications, dependent: :destroy
 
   attachment :image
 end
