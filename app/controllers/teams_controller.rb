5062499f97a673a515f84e234bb1ac35d9dafb45
EC2 Default User <kapibara.bba0924@gmail.com>
2021-01-03 13:19:27 +0000

    [Add]

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -24,6 +24,10 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:team_id])
     @post_comment = PostComment.new
     @post_comments = @team.post_comments
+    @notifications = current_user.passive_notifications
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
   end
 
   def create
