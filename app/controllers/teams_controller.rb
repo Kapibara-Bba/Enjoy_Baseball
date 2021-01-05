6d9f253cb6296c2236578380a6a07213b4e7c36b
EC2 Default User <kapibara.bba0924@gmail.com>
2021-01-05 07:38:33 +0000

    [Add]通知機能

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -24,6 +24,7 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:team_id])
     @post_comment = PostComment.new
     @post_comments = @team.post_comments
+    # 通知機能
     @notifications = current_user.passive_notifications
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,7 +2,7 @@ class PostComment < ApplicationRecord
 
   belongs_to :user, optional: true
   belongs_to :team, optional: true
-  has_many :notifications, dependent: :destroy
+  has_many :notifications, foreign_key: 'post_comment_id', dependent: :destroy
 
   attachment :image
 
